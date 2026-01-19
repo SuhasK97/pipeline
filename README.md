ced383b24828066da460fcc15655336d4e4c1cd1
Ubuntu <ubuntu@ip-172-31-18-252.ec2.internal>
2026-01-19 10:25:09 +0000

    Add CMake C project

---
+++ README.md
@@ -0,0 +1,4 @@
+# CMake Pipeline Project
+
+Simple C project built using CMake.
+Used for Jenkins pipeline practice.
---
+++ build/CMakeFiles/cmake_pipeline.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_pipeline.dir/src/main.c.o"
+  "CMakeFiles/cmake_pipeline.dir/src/main.c.o.d"
+  "cmake_pipeline"
+  "cmake_pipeline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake_pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
